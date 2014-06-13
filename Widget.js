66a3cb784714bef95ed268c1a7f0205dba1c7363
Michael <mluubert@esri.ca>
2014-06-13 12:19:30 -0400

    merge

--- Widget.js
+++ Widget.js
@@ -1957,6 +1957,11 @@ define(
             ook.remove();
           }
         }));
+
+      },
+
+      formIsValid: function() {
+
         if (registry.byId("firstName").isValid() && registry.byId("lastName").isValid() && registry.byId("loginEmail").isValid()) {
           return true;
         } else {
